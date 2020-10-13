e0109450c56d45f21a69b75f88f5c921197a3fab
cjarv <cjarvfx@gmail.com>
2020-10-13 10:41:55 -0700

    styling app

---
+++ client/blog/src/components/app_navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="nav-container">
+        <div class="nav-icon"></div>
+        <div class="navbar">
+            <div class="nav-btn center">Map</div>
+            <div class="nav-btn center">Tracker</div>
+            <div class="nav-btn center">History</div>
+            <div class="nav-btn center">Blog</div>
+            <div class="nav-btn center">Message</div>
+            <div class="nav-btn center">Help</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "app_navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
